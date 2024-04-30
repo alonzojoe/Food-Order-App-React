00f3a3ce89a7ebb9dd87b31a4244a37ac4272449
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-04-30 11:07:31 +0800

    Create meals component and its part

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 import "./style.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
