7356405b1bc50bdd27322bcc47457b85967a111f
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-04-30 10:30:39 +0800

    Creating a button cart component and replace static button in header component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import MealImage from "../../assets/images/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React + Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={MealImage} alt="Meals-Menu" />
